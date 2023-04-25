c0d36c4e8f539e4fa631f4dcc978d66f3276b467
Tazoria <gayeonlee93@gmail.com>
2023-04-25 20:44:43 +0900

    로그인 화면 구성

---
+++ README.md
@@ -0,0 +1,2 @@
+# Sticker_On_Face_Vue
+This is a Vue.js project for Sticker_On_Face
---
+++ front/src/layouts/default/Constrained.vue
@@ -0,0 +1,104 @@
+<template>
+  <!-- 컴포넌트 추가시 할 일:
+    1. template: 넣고싶은 위치에 태그넣기
+    2. script: import + export default의 components에 추가
+  -->
+  <v-app id="inspire">
+    <v-app-bar flat>
+      <v-container class="fill-height d-flex align-center">
+        <v-avatar
+          class="me-10 ms-4"
+          color="grey-darken-1"
+          size="32"
+        ></v-avatar>
+
+        <v-btn
+          v-for="link in links"
+          :key="link"
+          variant="text"
+        >
+          {{ link }}
+        </v-btn>
+
+        <v-spacer></v-spacer>
+
+        <v-responsive max-width="260">
+          <v-text-field
+            density="compact"
+            hide-details
+            variant="solo"
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="bg-grey-lighten-3">
+      <v-container>
+        <v-row>
+          <v-col cols="2">
+            <v-sheet rounded="lg">
+              <v-list rounded="lg">
+                <v-list-item
+                  v-for="n in 5"
+                  :key="n"
+                  link
+                >
+                  <v-list-item-title>
+                    List Item {{ n }}
+                  </v-list-item-title>
+                </v-list-item>
+
+                <v-divider class="my-2"></v-divider>
+
+                <v-list-item
+                  link
+                  color="grey-lighten-4"
+                >
+                  <v-list-item-title>
+                    Refresh
+                  </v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-sheet>
+          </v-col>
+
+          <v-col>
+            <v-sheet
+              min-height="70vh"
+              rounded="lg"
+            >
+              <!-- 컴포넌트 추가 -->
+              <SignUp></SignUp>
+            </v-sheet>
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+// 수정이 매우~~~~~~~~ 필요함 특히 data부분!
+  import SignUp from '@/components/SignUp.vue';
+  export default {
+    data: () => ({
+      links: [
+        'Dashboard',
+        'Messages',
+        'Profile',
+        'Updates',
+      ],
+      username: '',
+      password: '',
+    }),
+      methods: {
+          login() {
+              // Your login logic here
+          },
+      },
+      components: {
+        // 추가된 컴포넌트 넣어주는 위치
+        SignUp
+      }
+  }
+</script>
