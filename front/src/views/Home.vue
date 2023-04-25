a3ffd15fef4f7fd982ff1733b055bfa6f8a98e78
Tazoria <gayeonlee93@gmail.com>
2023-04-25 11:35:46 +0900

    vue3 + vuetify3 - 컴포넌트 예시

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -7,5 +7,5 @@
 <script setup>
   // import HelloWorld from '@/components/HelloWorld.vue'
   // import SignUp from '@/components/SignUp.vue';
-  import Constrained from '@/layouts/default/constrained.vue';
+  import Constrained from '@/layouts/default/Constrained.vue';
 </script>
